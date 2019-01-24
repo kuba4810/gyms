ed6e99353c0fe194a1228c43969cea281e5f8e93
kocmax95 <maksymiliankoczaski@gmail.com>
2019-01-24 23:26:36 +0100

    aaa

--- src/Components/PersonalTrainers/PersonalTrainer.js
+++ src/Components/PersonalTrainers/PersonalTrainer.js
@@ -86,6 +86,40 @@ class Trainer extends Component {
                     isEmailConfirmed={this.props.user.emailConfirmed}
                     page={'TRENERZY'}/>
 
+                      <div className="jumbotron text-center bg-secondary" style={{marginBottom:0}}>
+                        <h3>Znajdz Profesjonalnych trenerow w twoim rejonie</h3>
+                        <h5>Pełne plany trenerow wraz z opiniami i cennikiem</h5>
+
+                        <div className="container-fluid mt-4 ">
+                          <div className="row">
+                            <div className="input-group col-lg-3  mb-3 mx-auto">
+                              <div className="input-group-prepend">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Wyszukaj</span>
+                              </div>
+                              <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                            </div>
+                          </div>
+                          </div>
+                          <button type="button" class="btn btn-outline-warning">Dodaj trenera</button>
+                      </div>  
+
+                      <h2 className="text-center p-4">Dostępni Trenerzy:</h2> 
+                      <div className="row container-fluid">
+                    
+
+                          <div className="col-md-6 col-xl-3 p-4">
+                          <div className="card">
+                          <img className="card-img-top" src="https://cdn1us.denofgeek.com/sites/denofgeekus/files/styles/main_wide/public/2016/07/arnold-schwarzenegger-pumping-iron.jpg?itok=ovqXmA3Y" alt="Card image cap"/>
+                              <div className="card-body">
+                              <h5 className="card-title">Arnold Schwarzenegger</h5>
+                              <p className="card-text">Najlepszy trener ever</p>
+                              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                              </div>
+                          </div>
+                          </div>
+
+                      </div>
+
                 
                 <Route exact path={this.props.match.path} component={TrainersListContainer} />
                 <Route exact path={`${this.props.match.path}/harmonogram/:training_id?`} component={SheduleContainer} />

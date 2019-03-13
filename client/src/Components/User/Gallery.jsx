878f695b6e60d0701f1a72fac620b2cfe01aa0b0
unknown <kuba__koziol@op.pl>
2019-03-13 10:47:34 +0100

    Gallery, images different size, arrows improoved

--- client/src/Components/User/Gallery.jsx
+++ client/src/Components/User/Gallery.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 class Gallery extends Component {
     state = {
         currentPhoto: null,
-        currentIndex : 0
+        currentIndex: 0
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setState({
             currentPhoto: this.props.photos[this.props.currentIndex].photo_name,
-            currentIndex : this.props.currentIndex
+            currentIndex: this.props.currentIndex
         })
 
-        console.log('Gallery mount',this.props);
+        console.log('Gallery mount', this.props);
     }
     // COMPONENT DID UPDATE
     // ------------------------------------------------------------------------
@@ -20,17 +20,17 @@ class Gallery extends Component {
             console.log(this.props.photos)
             this.setState({
                 currentPhoto: this.props.photos[this.props.currentIndex].photo_name,
-                currentIndex : this.props.currentIndex
-            },()=>{
+                currentIndex: this.props.currentIndex
+            }, () => {
                 console.log(this.state);
             })
         }
 
-        if(prev.currentIndex !== this.props.currentIndex){
+        if (prev.currentIndex !== this.props.currentIndex) {
             this.setState({
                 currentPhoto: this.props.photos[this.props.currentIndex].photo_name,
-                currentIndex : this.props.currentIndex
-            },()=>{
+                currentIndex: this.props.currentIndex
+            }, () => {
                 console.log(this.state);
             })
         }
@@ -38,23 +38,23 @@ class Gallery extends Component {
 
     // CHOOSE PHOTO
     // ------------------------------------------------------------------------
-    choosePhoto = (name,index) => {
+    choosePhoto = (name, index) => {
         this.setState({
-            currentPhoto : name,
-            currentIndex : index
+            currentPhoto: name,
+            currentIndex: index
         })
     }
 
     // NEXT IMAGE
     // ------------------------------------------------------------------------
-    nextImage = () =>{
+    nextImage = () => {
 
-        if(this.props.photos.length-1 > this.state.currentIndex){
+        if (this.props.photos.length - 1 > this.state.currentIndex) {
             let index = ++this.state.currentIndex;
             this.setState({
-                currentIndex : index,
-                currentPhoto : this.props.photos[index].photo_name
-            },()=>{
+                currentIndex: index,
+                currentPhoto: this.props.photos[index].photo_name
+            }, () => {
                 console.log(this.state);
             })
         }
@@ -63,13 +63,13 @@ class Gallery extends Component {
 
     // PREVIOUS IMAGE
     // ------------------------------------------------------------------------
-    prevImage = () =>{
+    prevImage = () => {
 
-        if(this.state.currentIndex > 0 ){
+        if (this.state.currentIndex > 0) {
             let index = --this.state.currentIndex;
             this.setState({
-                currentIndex : index,
-                currentPhoto : this.props.photos[index].photo_name
+                currentIndex: index,
+                currentPhoto: this.props.photos[index].photo_name
             })
         }
 
@@ -77,16 +77,16 @@ class Gallery extends Component {
 
     // HIDE GALLERY
     // ------------------------------------------------------------------------
-    hideGallery = () =>{
+    hideGallery = () => {
 
         let gallery = document.querySelector('.gallery');
 
         gallery.classList.remove('fadeIn');
         gallery.classList.add('fadeOut');
 
-        setTimeout(()=>{
+        setTimeout(() => {
             gallery.classList.add('invisible');
-        },500);
+        }, 500);
 
     }
 
@@ -96,43 +96,52 @@ class Gallery extends Component {
 
         let photos = this.props.photos.map((photo, index) => (
             <div className="userAvatar m-2 miniatureImage"
-            onClick={this.choosePhoto.bind(null,photo.photo_name,index)}>
+                onClick={this.choosePhoto.bind(null, photo.photo_name, index)}>
                 <img src={`http://localhost:8080/public/images/${photo.photo_name}.jpg`} />
             </div>
         ))
         let image = '';
 
         if (this.state.currentPhoto !== null) {
-            image = <img className=" w-100 h-100 " src={`http://localhost:8080/public/images/${this.state.currentPhoto}.jpg`} />
+            image = <img src={`http://localhost:8080/public/images/${this.state.currentPhoto}.jpg`} />
         }
         return (
             // Container
             <div class="gallery animated invisible">
 
                 <div className="close p-2" onClick={this.hideGallery}>
-                   <i className="fas fa-times text-danger"></i>
+                    <i className="fas fa-times text-danger"></i>
                 </div>
 
                 {/* Image container */}
-                <div className="bigImage mr-auto ml-auto w-50 h-50 position-relative mt-3 text-dark d-flex justify-content-center">
+                <div className="bigImage  h-50 position-relative mt-3 text-dark">
 
                     {/* Left arrow */}
-                    <div className="prevImage d-flex align-items-center"
-                    onClick={this.prevImage}>
-                        <i class="fas fa-angle-left"></i>
-                    </div>
+                    {
+                        this.state.currentIndex > 0 &&
+                        <div className="prevImage d-flex align-items-center"
+                            onClick={this.prevImage}>
+                            <i class="fas fa-angle-left"></i>
+                        </div>
+                    }
 
                     {/* Big image */}
                     {/* <div className="image w-50 h-50"> */}
-                        {image}
+                    {image}
                     {/* </div> */}
 
                     {/* Right arrow */}
-                    <div className="nextImage d-flex align-items-center"
-                    onClick={this.nextImage}>
-                        <i class="fas fa-angle-right"></i>
-                    </div>
+
+                    {
+                        this.state.currentIndex < this.props.photos.length-1 &&
+                        <div className="nextImage d-flex align-items-center"
+                            onClick={this.nextImage}>
+                            <i class="fas fa-angle-right"></i>
+                        </div>
+                    }
+
                 </div>
+
 
                 <div className="miniatureContainer d-flex justify-content-center">
                     {photos}
